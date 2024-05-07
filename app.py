f47ce19f3c4e991bd1062c859c60041f64fb57c9
pratama404 <agengputrapratama@gmail.com>
2024-05-07 19:21:17 +0700

    add visualisasi

--- app.py
+++ app.py
@@ -30,3 +30,57 @@ else:
         df = pd.read_excel("jabodetabek_house_price.xlsx")
     except FileNotFoundError:
         st.error("File 'jabodetabek_house_price.xlsx' tidak ditemukan.")
+
+if not df.empty:
+    st.write(df.head())
+
+    # Exploratory Data Analysis (EDA)
+    st.subheader("Exploratory Data Analysis (EDA)")
+
+    # Summary Statistics
+    st.write("Deskripsi Data:")
+    st.write(df.describe())
+
+    # Missing Values
+    st.write("Jumlah Missing Values per Kolom:")
+    st.write(df.isnull().sum())
+
+    #bar chart
+    st.subheader("Bar Chart")
+    bar_chart = px.bar(df, x='city', y='price_in_rp', title='Harga Properti di Jabodetabek', labels={'city': 'Kota/Kabupaten', 'price_in_rp': 'Harga (IDR)'})
+    st.plotly_chart(bar_chart)
+
+    
+    # Visualisasi distribusi harga properti dalam rupiah
+    st.subheader("Distribusi Harga Properti")
+    hist_chart = px.histogram(df, x='price_in_rp', nbins=30, title='Distribusi Harga Properti')
+    st.plotly_chart(hist_chart)
+
+    # Visualisasi distribusi jumlah kamar tidur dan kamar mandi
+    st.subheader("Distribusi Jumlah Kamar Tidur dan Kamar Mandi")
+    bedroom_chart = px.histogram(df, x='bedrooms', title='Distribusi Jumlah Kamar Tidur')
+    bathroom_chart = px.histogram(df, x='bathrooms', title='Distribusi Jumlah Kamar Mandi')
+    st.plotly_chart(bedroom_chart)
+    st.plotly_chart(bathroom_chart)
+
+    # Visualisasi distribusi luas tanah dan luas bangunan
+    st.subheader("Distribusi Luas Tanah dan Luas Bangunan")
+    land_size_chart = px.histogram(df, x='land_size_m2', nbins=30, title='Distribusi Luas Tanah (m2)')
+    building_size_chart = px.histogram(df, x='building_size_m2', nbins=30, title='Distribusi Luas Bangunan (m2)')
+    st.plotly_chart(land_size_chart)
+    st.plotly_chart(building_size_chart)
+
+    # Menghapus kolom non-numerik
+    df_numeric = df.drop(columns=['url', 'title', 'address', 'district', 'city'])
+
+    # Mengubah tipe data kolom yang tersisa ke numerik
+    df_numeric = df_numeric.apply(pd.to_numeric, errors='coerce')
+
+    # Menghapus baris yang mengandung nilai NaN
+    df_numeric = df_numeric.dropna()
+
+    # Korelasi antara harga properti dengan property_condition
+    st.subheader("Korelasi antara Harga Properti dengan Kondisi Properti")
+    correlation = df_numeric[['price_in_rp', 'property_condition']].corr()
+    fig = px.imshow(correlation, labels=dict(x="Variabel", y="Variabel", color="Korelasi"), x=correlation.index, y=correlation.columns)
+    st.plotly_chart(fig)
