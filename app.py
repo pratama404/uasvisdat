c774aa211be51ce847c0fbf36a406fdfc137a33d
SinRyuRifal <rifqii.naufall37@gmail.com>
2024-05-08 03:31:10 +0700

    asdasd

--- app.py
+++ app.py
@@ -75,37 +75,23 @@ else:
     )
     df_filtered = df[mask & (df["facilities"] != "")]
 
-# Membuat input untuk harga minimum dalam miliar dan juta
-min_price_miliar, min_price_juta, max_price_miliar, max_price_juta = st.sidebar.columns(
-    4
-)
+# Membuat input untuk harga minimum dan maksimum dalam satuan miliar
+min_price_column, max_price_column = st.sidebar.columns(2)
 
-min_price_miliar = min_price_miliar.number_input(
-    "Harga Minimum (Miliar)", min_value=0, value=0
-)
-min_price_juta = min_price_juta.number_input(
-    "Harga Minimum (Juta)", min_value=0, max_value=999, value=0
-)
+min_price = min_price_column.number_input("Harga Minimum", min_value=0, value=0)
 
-max_price_miliar = max_price_miliar.number_input(
-    "Harga Maksimum (Miliar)",
-    min_value=0,
-    value=df["price_in_rp"].max() // 1_000_000_000,
-)
-max_price_juta = max_price_juta.number_input(
-    "Harga Maksimum (Juta)",
-    min_value=0,
-    max_value=999,
-    value=(df["price_in_rp"].max() % 1_000_000_000) // 1_000_000,
+max_price = max_price_column.number_input(
+    "Harga Maksimum", min_value=0, value=df["price_in_rp"].max() // 1_000_000
 )
 
 # Mengonversi input harga minimum dan maksimum ke dalam satuan rupiah
-min_price = min_price_miliar * 1_000_000_000 + min_price_juta * 1_000_000
-max_price = max_price_miliar * 1_000_000_000 + max_price_juta * 1_000_000
+min_price_rupiah = min_price * 1_000_000_000
+max_price_rupiah = max_price * 1_000_000_000
 
 # Menerapkan filter
-df_filtered = df[(df["price_in_rp"] >= min_price) & (df["price_in_rp"] <= max_price)]
-
+df_filtered = df[
+    (df["price_in_rp"] >= min_price_rupiah) & (df["price_in_rp"] <= max_price_rupiah)
+]
 
 advanced_filters_clicked = st.sidebar.button("Advanced Filters")
 if advanced_filters_clicked:
