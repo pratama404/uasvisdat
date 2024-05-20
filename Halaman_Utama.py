e19ff40b40806016da3fbfc975284934c85f767c
SinRyuRifal <rifqii.naufall37@gmail.com>
2024-05-20 12:57:33 +0700

    asdasdasd

---
+++ Halaman_Utama.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Jabodetabek House Price",
+    page_icon="🏠",
+)
+
+st.title("Latar Belakang")
+st.sidebar.success("Pilih Halaman")
---
+++ pages/About.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.text("Ageng Putra Pratama - 09010622001")
+st.text("Muhammad Rifqi Naufal Irsyad - 09010622010")
+st.text("Rizki Cahyani Fitonah - 09020622042")
