a3bb8c539d2426e365b3f12dbd7642b9f571c2f7
pratama404 <agengputrapratama@gmail.com>
2025-08-17 12:53:02 +0700

    Update property metrics labels for better clarity and visual appeal

--- Visualisasi_Data.py
+++ Visualisasi_Data.py
@@ -248,16 +248,16 @@ def Home(df_filtered):
     # Menampilkan summary
     total1, total2, total3, total4 = st.columns(4, gap="large")
     with total1:
-        st.info(":pencil: Jumlah Properti yang Dijual")
+        st.info("🏠 Jumlah Properti yang Dijual")
         st.metric(label="Count of Houses", value=f"{jumlah_rumah:.2f}")
     with total2:
-        st.info(":pencil: Rata-Rata Harga Properti")
+        st.info("📊 Rata-Rata Harga Properti")
         st.metric(label="Average of Price", value=f"${avg_harga_miliar:.2f}B")
     with total3:
-        st.info(":pencil: Harga Properti Tertinggi")
+        st.info("⬆️ Harga Properti Tertinggi")
         st.metric(label="Max of Price", value=f"${max_harga_miliar}B")
     with total4:
-        st.info(":pencil: Harga Properti Terdendah")
+        st.info("⬇️ Harga Properti Terdendah")
         st.metric(label="Min of Price", value=f"${min_harga_juta}M")
 
 
